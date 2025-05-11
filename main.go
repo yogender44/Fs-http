aa497bfbfa4a84be53c874772f15ee2d9f56e7a8
yogender44 <43621325+yogender44@users.noreply.github.com>
2025-05-11 19:04:44 +0000

    setup multithreaded http server which listens to connections

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
+
 import (
- "fmt"
- "net"
+	"fmt"
+	"log"
+	"net"
+	"time"
 )
 
 func main() {
-  listener, err := net.listen("tcp", ":1729")
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.println("Hello world!", listener)
+	listener, err := net.Listen("tcp", ":1729")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// to prevent server exiting once it has processed a connection
+	for {
+		conn, err := listener.Accept() // blocking
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Client connected ", conn)
+
+		go do(conn) // creates a go routine to handle conn processing on a separate thread.
+	}
+}
+
+func do(conn net.Conn) {
+	buf := make([]byte, 1024)
+
+	_, err := conn.Read(buf) // blocking
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Processing the request")
+	time.Sleep(8 * time.Second)
+
+	conn.Write([]byte("HTTPS/1.1 200 OK\r\nHello World!\r\n")) // blocking system call
+	conn.Close()
+  // TCP backlog queue configuration
 }
